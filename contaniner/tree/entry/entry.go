9987a2c05a9228016bd796e0c3678bb165f02e18
ansuns <251024883@qq.com>
2018-09-27 16:28:51 +0800

    基础

--- contaniner/tree/entry/entry.go
+++ contaniner/tree/entry/entry.go
@@ -5,9 +5,9 @@ import "project_google_gostudy/contaniner/tree"
 func main() {
 	var root tree.Node
 
-	root = tree.Node{Value:3}
-	root.Left = &tree.Node{}
-	root.Right = &tree.Node{5, nil, nil}
+	root = tree.Node{Value:3} //3
+	root.Left = &tree.Node{} //0
+	root.Right = &tree.Node{5, nil, nil} //5
 	root.Right.Left = new(tree.Node)
 	root.Left.Right = tree.CreateNode(2)
 	root.Right.Left.SetValue(4)
