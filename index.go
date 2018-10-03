a46136a2a379daa7569f01b6c7bc10fbc461c57b
ansuns <251024883@qq.com>
2018-10-03 16:03:24 +0800

    统一目录

--- index.go
+++ index.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+)
+
 func main() {
-	$END$
+
+	for skip := 0; ; skip++{
+		pc, file, line, ok := runtime.Caller(skip)
+		if !ok {
+			break
+		}
+		fmt.Printf("skip = %v, pc = %v, file = %v, line = %v\n", skip, pc, file, line)
+	}
 }
